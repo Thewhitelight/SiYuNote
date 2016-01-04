2056e212606f69cb9f0be7eff399ba1d30b3cbb6
Libery <921618920@qq.com>
2016-01-04 21:57:16 +0800

    update

--- app/src/main/java/cn/libery/siyunote/ui/AddNoteActivity.java
+++ app/src/main/java/cn/libery/siyunote/ui/AddNoteActivity.java
@@ -178,38 +178,6 @@ public class AddNoteActivity extends BaseActivity {
 
     };
 
-/*    *//**
-     * 参数设置
-     *//*
-    public void setParam() {
-        // 清空参数
-       // mIat.setParameter(SpeechConstant.PARAMS, null);
-
-        // 设置听写引擎
-        mIat.setParameter(SpeechConstant.ENGINE_TYPE, SpeechConstant.TYPE_CLOUD);
-        // 设置返回结果格式
-        mIat.setParameter(SpeechConstant.RESULT_TYPE, "json");
-        // 设置语言
-        mIat.setParameter(SpeechConstant.LANGUAGE, "zh_cn");
-        // 设置语言区域
-        mIat.setParameter(SpeechConstant.ACCENT, "zh_cn");
-
-
-        // 设置语音前端点:静音超时时间，即用户多长时间不说话则当做超时处理
-        mIat.setParameter(SpeechConstant.VAD_BOS, "4000");
-
-        // 设置语音后端点:后端点静音检测时间，即用户停止说话多长时间内即认为不再输入， 自动停止录音
-        mIat.setParameter(SpeechConstant.VAD_EOS, "1000");
-
-        // 设置标点符号,设置为"0"返回结果无标点,设置为"1"返回结果有标点
-        mIat.setParameter(SpeechConstant.ASR_PTT, "1");
-
-        // 设置音频保存路径，保存音频格式支持pcm、wav，设置路径为sd卡请注意WRITE_EXTERNAL_STORAGE权限
-        // 注：AUDIO_FORMAT参数语记需要更新版本才能生效
-        mIat.setParameter(SpeechConstant.AUDIO_FORMAT, "wav");
-        mIat.setParameter(SpeechConstant.ASR_AUDIO_PATH, Environment.getExternalStorageDirectory() + "/msc/iat.wav");
-    }*/
-
     private void initData() {
         if (timestamp != 0) {
             EventRecord eventRecord = EventRecord.getByTimeStamp(timestamp);
